36b98ea7c8502a6b6013631ed4d5651e14e387e5
hyunzsu <kkm5169@naver.com>
2023-01-16 11:31:38 +0900

    [추가] 배열 구조 분해 할당

--- client/main.js
+++ client/main.js
@@ -1,14 +1,27 @@
-// [ 주사위 굴리기]
+// [ 주사위 굴리기] [1 ~ 8]
 // 1. dice 애니메이션 불러오기
 // 2. bindEvent 유틸함수 만들기
 // 3. handleRollingDice 함수 만들고 토글로 애니메이션 제어하기
 // 4. 변수 보호를 위한 클로저 + IIFE 사용하기
 
+// [ 레코드 리스트 보이기 ] [9 ~ ]
+// 1. handleRecord 함수를 만들기
+// 2. disable 활성 유틸 함수 만들기
+// 3. handleReset 함수를 만듭니다.
+// 4. visible 활성 유틸 함수 만들기
+// 5. toggleState 유틸 함수 만들기 
 
 
-import { diceAnimation, getNode } from './lib/index.js';
 
-const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)'); // (1) 버튼 그룹안에있는 친자식 첫번째
+
+import { diceAnimation, getNode, getNodes } from './lib/index.js';
+
+// (11) 배열의 구조 분해 할당
+const [rollingDiceButton, recordButton, resetButton] = getNodes('.buttonGroup > button');
+
+/* const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)'); // (1) 버튼 그룹안에있는 친자식 첫번째
+const recordButton = getNode('.buttonGroup > button:nth-child(2)'); // (9) 기록 버튼 변수설정
+const resetButton = getNode('.buttonGroup > button:nth-child(3)'); */
 
 
 // (2) 이벤트핸들러 연결
@@ -20,8 +33,10 @@ const handlerRollingDice = (() => {
   return () => { // (7) 보호하기 위해 클로저 사용
     if(!isRolling) {
       stopAnimation = setInterval(diceAnimation,100) // (3) 애니메이션 구현해놓은거 연결
+      recordButton.disabled = true; // (10) hidden 느낌, 값을 제어할 수 있음
     } else{
       clearInterval(stopAnimation); // (5) 지울 수 있음. 아이디 값 받아서, 그 아이디 값을 받아서 필요할 때 종료시킬 수 있음
+      recordButton.disabled = false;
     }
   
     isRolling = !isRolling; // 현재 false 그러고 true로 바뀜, 다시 클릭하면 else문 실행, 또 반전 false
